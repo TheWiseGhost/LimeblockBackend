1b1e94d37c062e8eb85da43ad0604fd0d0ed9986
Aditya Byju <byjuaditya@gmail.com>
2025-02-27 22:11:11 -0600

    Fix user details url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main, create_user, sign_in
+from .views import main, create_user, sign_in, user_details
 
 urlpatterns = [
     path('', main, name='main'),
     path('create_user/', create_user, name='create_user'),
-    path('sign_in/', sign_in, name='create_user'),
+    path('sign_in/', sign_in, name='sign_in'),
+    path('user_details/', user_details, name='user_details'),
 ]
 
